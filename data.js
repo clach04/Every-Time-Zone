6197d8be0b767b424111110cc616fc5a86d65300
clach04 <clach04@gmail.com>
2021-12-28 21:32:40 -0800

    refactor data array/list
    reduce list

--- data.js
+++ data.js
@@ -1,1 +1,9 @@
-var data = [[-8.0,"Los Angeles","PST UTC-8",true],[-7.0,"Denver","MST UTC-7",true],[-6.0,"Cedar Rapids","CST UTC-6",true],[-5.0,"New York","EST UTC-5",true],[-3.0,"Rio de Janeiro","BRT UTC-3",true]];
+var data = [
+            [13.0,"Auckland","NZDT UTC+13",true],
+            [11.0,"Sydney","EST UTC+11",true],
+            [8.0,"Singapore","SGT UTC+8",true],
+            [5.5,"Bangalore","IST UTC+5.5",true],
+            [0.0,"Reading, UK","GMT UTC",true],
+            [-7.0,"Denver","MST UTC-7",true],
+            [-8.0,"San Francisco","PST UTC-8",true]
+        ];
