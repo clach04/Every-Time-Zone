b785cbf3332d5aba0b17a9cd52b4d7cc66d10074
clach04 <clach04@gmail.com>
2025-10-13 10:07:18 +0800

    Doc hard coded (DST) timezones

--- data.js
+++ data.js
@@ -9,6 +9,8 @@ var data = [
             [11.0,"Sydney","EST UTC+11",true],
             [8.0,"Singapore","SGT UTC+8",true],
             [5.5,"Bangalore","IST UTC+5.5",true],
+            // Below are Winter time, NOT Summer (DST) time
+            // TODO either update a few times a year, or use DST aware rules
             [1.0,"Europe","CET UTC+1",true],
             [0.0,"United Kingdom","GMT UTC",true],
             [-5.0,"New York","EST UTC-5",true],
