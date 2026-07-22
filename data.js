6a4e06bc8dbc1a691fe92739833dffa3c42ac640
clach04 <clach04@gmail.com>
2026-07-21 21:43:51 -0700

    Update UK TZ based on https://www.gov.uk/when-do-the-clocks-change

--- data.js
+++ data.js
@@ -8,7 +8,7 @@ var data = [
     ["SGT-8", "Singapore", "SGT", true],
     ["IST-5:30", "Bangalore", "IST", true],
     ["CET-1CEST,M3.5.0,M10.5.0/3", "Europe", "CET", true],
-    ["GMT0BST,M3.5.0,M10.5.0", "United Kingdom", "UK", true],
+    ["GMT0BST,M3.5.0/1:00:00,M10.5.0/2:00:00", "United Kingdom", "UK", true],
     ["EST5EDT,M3.2.0,M11.1.0", "New York", "US East", true],
     ["CST6CDT,M3.2.0,M11.1.0", "Austin", "US Central", true],
     ["MST7MDT,M3.2.0,M11.1.0", "Denver", "US Mountain", true],
